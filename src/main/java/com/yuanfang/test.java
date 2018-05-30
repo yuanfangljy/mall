0d1a11010ee4ba1cbe40eb2f0d65a9458c5733a7
yuanfangljy <952992956@qq.com>
2018-05-30 23:31:46 +0800

    测试

--- src/main/java/com/yuanfang/test.java
+++ src/main/java/com/yuanfang/test.java
@@ -1,5 +1,9 @@
 package com.yuanfang;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class test {
     public static void main(String[] args) {
         System.out.println("hello word!");
